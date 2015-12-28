8916f68df9fbaa4ae293f08c61f387bf1dfabbe5
Woohyun Kim <r45r54r45@naver.com>
2015-12-28 16:15:42 +0900

    little change

---
+++ application/controllers/Search.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Search extends CI_Controller {
+
+  public function gosu(){
+    $this->load->view('gosu_search');
+
+  }
+  public function how(){
+
+
+  }
+  public function cs(){
+
+
+  }
+
+}
---
+++ application/controllers/Service.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Service extends CI_Controller {
+
+  public function desc(){
+    $this->load->view('welcome_message');
+    
+  }
+  public function how(){
+
+
+  }
+  public function cs(){
+
+
+  }
+
+}
