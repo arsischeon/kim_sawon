9a50a9a7186c68d2cd9d376723cf3212be4ecaca
Woohyun Kim <r45r54r45@naver.com>
2015-12-28 19:55:55 +0900

    fewfwe

---
+++ application/views/header.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+  <script src=https://code.jquery.com/jquery-2.1.4.min.js"></script>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body style="margin:0">
--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,18 +1,4 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Welcome to CodeIgniter</title>
-</head>
-<body style="margin:0">
 
 <div>
 	<object data="/sources/mainpage.svg" type="image/svg+xml"></object>
 </div>
-
-
-</body>
-</html>
