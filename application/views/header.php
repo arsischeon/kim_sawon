90376537ae251731569ee955b82f4aa8fee65681
Woohyun Kim <r45r54r45@naver.com>
2015-12-28 19:59:44 +0900

    cwecvwev

--- application/views/header.php
+++ application/views/header.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
-  <script src=https://code.jquery.com/jquery-2.1.4.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title></title>
