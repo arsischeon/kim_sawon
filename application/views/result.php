620296192bafea5ff38b10cd30161640ea4c6456
Woohyun Kim <r45r54r45@naver.com>
2015-12-29 20:18:03 +0900

    view 수정

--- application/views/result.php
+++ application/views/result.php
@@ -1,20 +1,28 @@
 
 <table>
+  <tr>
+    <td>수량 </tr>
+    <td>가격 </tr>
+  </tr>
 <?php foreach ($resultExact->result_array() as $result): ?>
-  <td>
-    <tr><? echo $result['amount']; ?></tr>
-    <tr><? echo $result['price']; ?></tr>
-  </td>
+  <tr>
+    <td><? echo $result['amount']; ?></tr>
+    <td><? echo $result['price']; ?></tr>
+  </tr>
 <?php endforeach; ?>
 </table>
 
 <hr>
 
 <table>
+  <tr>
+    <td>수량 </tr>
+    <td>가격 </tr>
+  </tr>
 <?php foreach ($resultSimmilar->result_array() as $result): ?>
-  <td>
-    <tr><? echo $result['amount']; ?></tr>
-    <tr><? echo $result['price']; ?></tr>
-  </td>
+  <tr>
+    <td><? echo $result['amount']; ?></tr>
+    <td><? echo $result['price']; ?></tr>
+  </tr>
 <?php endforeach; ?>
 </table>
