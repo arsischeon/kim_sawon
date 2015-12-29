5afb776c7b12c1cbde9ff6128bf0a640300ac51c
Woohyun Kim <r45r54r45@naver.com>
2015-12-29 20:19:13 +0900

    minor mistake

--- application/views/result.php
+++ application/views/result.php
@@ -1,28 +1,28 @@
-
+<h1>정확한 결과</h1>
 <table>
   <tr>
-    <td>수량 </tr>
-    <td>가격 </tr>
+    <td>수량 </td>
+    <td>가격 </td>
   </tr>
 <?php foreach ($resultExact->result_array() as $result): ?>
   <tr>
-    <td><? echo $result['amount']; ?></tr>
-    <td><? echo $result['price']; ?></tr>
+    <td><? echo $result['amount']; ?></td>
+    <td><? echo $result['price']; ?></td>
   </tr>
 <?php endforeach; ?>
 </table>
 
 <hr>
-
+<h1>추천결과</h1>
 <table>
   <tr>
-    <td>수량 </tr>
-    <td>가격 </tr>
+    <td>수량 </td>
+    <td>가격 </td>
   </tr>
 <?php foreach ($resultSimmilar->result_array() as $result): ?>
   <tr>
-    <td><? echo $result['amount']; ?></tr>
-    <td><? echo $result['price']; ?></tr>
+    <td><? echo $result['amount']; ?></td>
+    <td><? echo $result['price']; ?></td>
   </tr>
 <?php endforeach; ?>
 </table>
