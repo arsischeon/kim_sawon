c66954df7c28719cbb0240f09587e8e07a5e852b
Woohyun Kim <r45r54r45@naver.com>
2015-12-29 19:41:23 +0900

    검색함수 v1

--- application/controllers/Result.php
+++ application/controllers/Result.php
@@ -5,10 +5,12 @@ class Result extends CI_Controller {
 
   public function gosoo(){
     $this->load->model('search');
-    $resultExact=$this->search->gosuExact();
-    $resultSimmilar=$this->search->gosuSimillar();
-    
+    $data['resultExact']=$this->search->gosuExact();
+    $data['resultSimmilar']=$this->search->gosuSimillar();
 
+    $this->load->view('header');
+    $this->load->view('result');
+    $this->load->view('footer');
 
   }
   public function joongsoo(){
--- application/models/Search.php
+++ application/models/Search.php
@@ -1,13 +1,80 @@
+<?php
 class Search extends CI_Model{
+  $goods;
+  $size;
+  $side;
+  $paperType;
+  $paperThick;
+  $color;
+  $coating;
+  $amount;
+  public function __construct()
+        {
+          $this->load->database();
+        }
+  private function getInfo(){
+    $goods=$this->input->get('goods');
+    $size=$this->input->get('size');
+    $side=$this->input->get('side');
+    $paperType=$this->input->get('paperType');
+    $paperThick=$this->input->get('paperThick');
+    $color=$this->input->get('color');
+    $coating=$this->input->get('coating');
+    $amount=$this->input->get('amount');
 
+  }
   public function gosuExact(){
-      $this->input->get('');
+      getInfo();
+      return $this->db->query("
+select * from PARSED p
+join SITE s in p.site_id=s.id
+where p.site_id in (select id from SITE
+where
+goods like '$goods'
+and size like '$size'
+and side like '$side'
+and paperType like '$paperType'
+and paperThick like '$paperThick'
+and color like '$color'
+and coating like '$coating')
+and goods = '$goods'
+and size = '$size'
+and side = '$side'
+and paperType = '$paperType'
+and paperThick = '$paperThick'
+and color = '$color'
+and coating = '$coating'
+and amount = '$amount'
+order by price
+");
 
       return $this->db->query('');
 
   }
   public function gosuSimmilar(){
-      $this->input->post('title');
+    getInfo();
+    return $this->db->query("
+select * from PARSED p
+join SITE s in p.site_id=s.id
+where p.site_id in (select id from SITE
+where
+goods like '$goods'
+and size like '$size'
+and side like '$side'
+and paperType like '$paperType'
+and paperThick like '$paperThick'
+and color like '$color'
+and coating like '$coating')
+and goods = '$goods'
+and size = '$size'
+and side = '$side'
+and paperType = '$paperType'
+and paperThick = '$paperThick'
+and color = '$color'
+and coating = '$coating'
+and amount > '$amount'
+order by price
+");
 
 
   }
