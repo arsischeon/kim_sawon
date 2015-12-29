45de839b184de48d2fbc5975de29388cac4bf1d9
Woohyun Kim <r45r54r45@naver.com>
2015-12-29 11:28:43 +0900

    고수페이지 헤더 슬라이드 완성

--- application/views/gosu_search.php
+++ application/views/gosu_search.php
@@ -6,26 +6,39 @@
 </div>
 
 <style>
-@keyframes example {
+@keyframes down {
     from {top:-195px;}
     to {top:0px;}
-
 }
-.moving{
+@keyframes up {
+    from {top:0px;}
+		to {top:-195px;}
+}
+.movingDown{
 	position:absolute;
-	animation-name: example;
+	animation-name: down;
 	animation-duration: 1s;
 	top:0px;
 }
-
+.movingUp{
+	position:absolute;
+	animation-name: up;
+	animation-duration: 1s;
+	top:-195px;
+}
 </style>
 
 <script>
 
-document.getElementById('obj2').addEventListener("load",function(){
+document.getElementById('obj').addEventListener("load",function(){
 	var doc=this.getSVGDocument();
 	doc.querySelector('#previous_page_arrow').addEventListener("mouseover",function(){
-		document.getElementById('obj2').classList.add('moving');
+		document.getElementById('obj2').classList.add('movingDown');
+		document.getElementById('obj2').addEventListener("mouseout",function(){
+			document.getElementById('obj2').classList.remove('movingDown');
+			document.getElementById('obj2').classList.add('movingUp');
+		});
+					document.getElementById('obj2').classList.remove('movingUp');
 		// var count=-195;
 		// while(count<0){
 		// 	count+=0.01;
