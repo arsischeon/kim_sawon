1b9aa7de4b01358108b0daa9d42b598a11eb26a9
Woohyun Kim <r45r54r45@naver.com>
2015-12-30 15:17:44 +0900

    IE 호환성 체크2

--- application/views/header.php
+++ application/views/header.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta http-equiv="X-UA-Compatible" content="IE=EmulateIE9,chrome=1">
   <meta charset="utf-8">
   <meta name="description" content="프린트 최저가 검색">
   <meta name="author" content="SERIESC">
