32b4e6b96134be896bc43cdf68be43ccaf81fec0
Woohyun Kim <r45r54r45@naver.com>
2015-12-30 15:07:46 +0900

    IE 호환성 해결

--- application/views/header.php
+++ application/views/header.php
@@ -5,6 +5,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <head>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 	<meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta name="description" content="프린트 최저가 검색">
+  <meta name="author" content="SERIESC">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title></title>
 </head>
