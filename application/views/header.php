bd8716c8cf7baf1b797a1f3b6ea001bab663e6da
Woohyun Kim <r45r54r45@naver.com>
2015-12-30 15:15:02 +0900

    IE 호환성 체크

--- application/views/header.php
+++ application/views/header.php
@@ -3,12 +3,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-	<meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta charset="utf-8">
   <meta name="description" content="프린트 최저가 검색">
   <meta name="author" content="SERIESC">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 	<title></title>
 </head>
 <body style="margin:0">
