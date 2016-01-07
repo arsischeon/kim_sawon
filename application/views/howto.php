84bb38983e54ad1938a7c9a14158c59b3748eb6f
Woohyun Kim <r45r54r45@naver.com>
2016-01-07 13:51:15 +0900

    ddd

--- application/views/howto.php
+++ application/views/howto.php
@@ -18,31 +18,6 @@
 var obj=document.getElementById('obj');
 var obj2=document.getElementById('obj2');
 
-function getRule(data) {
-
-      var rule;
-
-      var ss = document.styleSheets;
-
-      for (var i = 0; i < ss.length; ++i) {
-
-          // loop through all the rules!
-
-          for (var x = 0; x < ss[i].cssRules.length; ++x) {
-
-              rule = ss[i].cssRules[x];
-
-              if (rule.name == data || rule.selectorText=="."+data) {
-
-                  return rule;
-
-              }
-
-          }
-
-      }
-
-  }
 
 
 
