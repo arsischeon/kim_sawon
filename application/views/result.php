a681c9c671215ef630d7ee43325f2398350216a7
Woohyun Kim <r45r54r45@naver.com>
2016-01-08 16:11:10 +0900

    ㄹㄷㅈ

--- application/views/result.php
+++ application/views/result.php
@@ -127,7 +127,7 @@ $q2=new SplQueue();
  			 $resultq=$q2->dequeue();
  			 ?>
  		<tr>
- 			<td style="width:29%;font-size:10pt; text-align:left;"><b><? echo $resultq['amount']."장"; ?></b></td>
+ 			<td style="width:29%;font-size:10pt; text-align:left;"><b><? echo $resultq['amount']."장으로 바꿨을 때"; ?></b></td>
  			<td style="width:13%; font-size:10pt; text-align:left;"><? echo $resultq['site_name']; ?></td>
 				<td style="width:10%; font-size:10pt; text-align:left;"><? echo number_format($resultq['price']/$resultq['amount'])."원"; ?></td>
 
