9f5e655b48feecd516c41a9cd5d7dd026ebd6a39
Woohyun Kim <r45r54r45@naver.com>
2016-01-12 14:10:25 +0900

    ewfewf

--- application/views/browser.php
+++ application/views/browser.php
@@ -1,10 +1,19 @@
 <div class="container">
-  <div class="img img-responsive" style="margin-top:20px; margin-bottom:20px;">
-    <image src="/sources/chuljang.png">
+    <div class="row">
+      <div class="col-xs-3">
+      </div>
+      <div class="col-xs-6">
+
+    <image style="margin-top:20px; margin-bottom:20px;" class="img img-responsive" src="/sources/chuljang.png">
+
+</div>
+  <div class="col-xs-3">
   </div>
+</div>
   <div class="row">
     <div class="col-xs-12">
-      <h2 style="text-align:center;">현재 사용하고 계신 브라우저에서는 이용할 수 없습니다.<br> 크롬브라우저로 접속하시면 모든 기능을 이용할 수 있습니다.</h2>
+      <h2 style="text-align:center; color:#66635A;">현재 사용하고 계신 브라우저에서는 이용할 수 없습니다.</h2>
+      <h2 style="text-align:center; color:#66635A;">  크롬브라우저로 접속하시면 모든 기능을 이용할 수 있습니다.</h2>
     </div>
   </div>
 </div>
