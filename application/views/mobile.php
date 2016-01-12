b0ab24b5f0d616dba68567da0b45994de033590a
Woohyun Kim <r45r54r45@naver.com>
2016-01-12 14:20:04 +0900

    dqw

--- application/views/mobile.php
+++ application/views/mobile.php
@@ -10,7 +10,7 @@
     <div class="col-xs-12">
       <h2 style="text-align:center; color:#66635A;">모바일 페이지는 현재 개발 중입니다.</h2>
       <h2 style="text-align:center; color:#66635A;">데스크탑으로 접속하시면 모든 기능을 이용할 수 있습니다.</h2>
-    <h2 style="text-align:center; color:red;">  <a href="http://plus.kakao.com/home/%40%EA%B9%80%EC%82%AC%EC%9B%90%ED%94%84%EB%A6%B0%ED%8A%B8%EC%A2%80%ED%95%B4%EC%99%80">김사원과 카톡하기</a></h2>
+    <h2 style="text-align:center; color:red;">  <a href="http://plus.kakao.com/home/@김사원프린트좀해와">김사원과 카톡하기</a></h2>
     </div>
   </div>
 
