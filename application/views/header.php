64a93668abfdba28d50566af0f2a5583d78c6067
Woohyun Kim <r45r54r45@naver.com>
2016-01-12 12:54:59 +0900

    구글 애널리틱스

--- application/views/header.php
+++ application/views/header.php
@@ -26,8 +26,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha256-KXn5puMvxCw+dAYznun+drMdG1IFl3agK0p/pqT9KAo= sha512-2e8qq0ETcfWRI4HJBzQiA3UoyFk6tbNyG+qSaIBZLyW9Xf3sWZHN/lxe9fTh1U45DpPf07yj94KsUHHWe4Yk1A==" crossorigin="anonymous"></script>
   <link href='https://cdn.rawgit.com/theeluwin/NotoSansKR-Hestia/master/stylesheets/NotoSansKR-Hestia.css' rel='stylesheet' type='text/css' crossorigin="anonymous">
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-	<title></title>
+    ga('create', 'UA-72322940-1', 'auto');
+    ga('send', 'pageview');
+
+  </script>
+	<title>김사원프린트좀해와</title>
   <style>
   html {
     -webkit-text-size-adjust: none
