245502814ab6aea0ad66503e1876d18ddaf4c5d2
Woohyun Kim <r45r54r45@naver.com>
2016-01-12 13:34:17 +0900

    dsv

--- application/views/mobile.php
+++ application/views/mobile.php
@@ -1,10 +1,10 @@
 <div class="container">
-  <div class="img img-responsive">
+  <div class="img img-responsive" style="margin-top:20px; margin-bottom:20px;">
     <image src="/sources/chuljang.png">
   </div>
   <div class="row">
     <div class="col-xs-12">
-      <h3 style="text-align:center;">모바일 페이지는 현재 개발 중입니다. 데스크탑으로 접속해주세요.</h3>
+      <h2 style="text-align:center;">모바일 페이지는 현재 개발 중입니다. 데스크탑으로 접속해주세요.</h2>
     </div>
   </div>
 </div>
