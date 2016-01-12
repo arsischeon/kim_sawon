76b91d71930b56115427b18f1cb2418394579099
Woohyun Kim <r45r54r45@naver.com>
2016-01-12 13:55:33 +0900

    faq

--- application/views/faq.php
+++ application/views/faq.php
@@ -6,50 +6,371 @@
 </div>
 <div id="cartoon" style="overflow-y:auto; position: absolute; z-index:1;">
 	<div class="container-fluid">
+
+
+		<ul class="nav nav-tabs">
+  <li class="active"><a data-toggle="tab" href="#home">일반</a></li>
+  <li><a data-toggle="tab" href="#menu1">주문</a></li>
+  <li><a data-toggle="tab" href="#menu2">결과</a></li>
+	<li><a data-toggle="tab" href="#menu3">제휴</a></li>
+</ul>
+
+<div class="tab-content">
+  <div id="home" class="tab-pane fade in active">
+
 		<div class="panel-group" id="accordion">
   <div class="panel panel-default">
     <div class="panel-heading">
       <h4 class="panel-title">
         <a data-toggle="collapse" data-parent="#accordion" href="#collapse1">
-        Collapsible Group 1</a>
+					<김사원프린트좀해와>는 무슨 서비스야?
+				</a>
       </h4>
     </div>
     <div id="collapse1" class="panel-collapse collapse in">
-      <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-      minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat.</div>
+      <div class="panel-body">
+				<김사원프린트좀해와>는 인쇄 최저가 업체를 찾아주는 검색 서비스야. 인쇄 실력에 따라 적합한 검색 방법을 이용할 수 있어!
+      </div>
     </div>
   </div>
   <div class="panel panel-default">
     <div class="panel-heading">
       <h4 class="panel-title">
         <a data-toggle="collapse" data-parent="#accordion" href="#collapse2">
-        Collapsible Group 2</a>
+					'인쇄 고수'는 어떤 사람이야?
+				</a>
       </h4>
     </div>
     <div id="collapse2" class="panel-collapse collapse">
-      <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-      minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat.</div>
+      <div class="panel-body">
+      	인쇄 고수는 인쇄 옵션이라면 줄줄이 외우는 인쇄 숙련자야.
+      </div>
     </div>
   </div>
   <div class="panel panel-default">
     <div class="panel-heading">
       <h4 class="panel-title">
         <a data-toggle="collapse" data-parent="#accordion" href="#collapse3">
-        Collapsible Group 3</a>
+					'인쇄 중수'는 어떤 사람이야?
+				</a>
       </h4>
     </div>
     <div id="collapse3" class="panel-collapse collapse">
-      <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-      minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat.</div>
+      <div class="panel-body">
+				인쇄 중수는 인쇄 옵션을 줄줄이 꿰뚫고 있지는 않지만 조금의 도움으로 충분히 검색이 가능해.
+
+      </div>
+    </div>
+  </div>
+	<div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapse4">
+					'인쇄 하수'는 어떤 사람이야?
+				</a>
+      </h4>
+    </div>
+    <div id="collapse4" class="panel-collapse collapse">
+      <div class="panel-body">
+				인쇄 하수는 인쇄에 대해 전혀 모르는 까막눈이야.
+
+      </div>
     </div>
   </div>
 </div>
+
+
+  </div>
+  <div id="menu1" class="tab-pane fade">
+
+		<div class="panel-group" id="accordion2">
+	<div class="panel panel-default">
+		<div class="panel-heading">
+			<h4 class="panel-title">
+				<a data-toggle="collapse" data-parent="#accordion2" href="#collapse5">
+					종이 재질은 어떻게 달라?
+				</a>
+			</h4>
+		</div>
+		<div id="collapse5" class="panel-collapse collapse">
+			<div class="panel-body">
+
+				종이 재질은 총가격에 영향을 크게 미치는 요소 중 하나야. 목적에 따라 보급형부터 고급형까지 선택할 수 있도록 6가지 옵션을 준비했어.
+			</div>
+		</div>
+	</div>
+	<div class="panel panel-default">
+		<div class="panel-heading">
+			<h4 class="panel-title">
+				<a data-toggle="collapse" data-parent="#accordion2" href="#collapse6">
+
+					종이 코팅의 용도는 뭐야?
+				</a>
+			</h4>
+		</div>
+		<div id="collapse6" class="panel-collapse collapse">
+			<div class="panel-body">
+
+				코팅은 인쇄 후에 표면에 막을 씌우는 작업을 하는 것을 의미해. 코팅을 하면 발색이 선명해지지! 코팅을 한 포스터는 눈에 잘 띄기 때문에 화장품이나 제품 포스터에 많이 사용해.
+			</div>
+		</div>
+	</div>
+	<div class="panel panel-default">
+		<div class="panel-heading">
+			<h4 class="panel-title">
+				<a data-toggle="collapse" data-parent="#accordion2" href="#collapse7">
+
+					종이 크기는 어떻게 달라?
+				</a>
+			</h4>
+		</div>
+		<div id="collapse7" class="panel-collapse collapse">
+			<div class="panel-body">
+
+				종이 크기는 용도나 예산에 따라서 다르게 선택하게 될거야. 만약 헷갈린다면 [인쇄 중수]나 [인쇄 하수]의 검색 페이지를 들어가봐. 키가 180cm인 고수와 160cm인 하수가 직접 각 사이즈를 들어보이니 이해가 쉬울거야!
+
+			</div>
+		</div>
+	</div>
+	<div class="panel panel-default">
+		<div class="panel-heading">
+			<h4 class="panel-title">
+				<a data-toggle="collapse" data-parent="#accordion2" href="#collapse8">
+
+					인쇄 도수는 어떻게 선택해?
+				</a>
+			</h4>
+		</div>
+		<div id="collapse8" class="panel-collapse collapse">
+			<div class="panel-body">
+
+				흑백은 1도, 색상은 4도, 특수색상(형광, 은색, 금색 등)이 사용되었다면 5도를 선택하면 돼. 하지만 흑백 중에서도 사진처럼 고화질의 작업물은 4도로 출력하는 것을 권장할게.
+
+			</div>
+		</div>
+	</div>
+	<div class="panel panel-default">
+		<div class="panel-heading">
+			<h4 class="panel-title">
+				<a data-toggle="collapse" data-parent="#accordion2" href="#collapse9">
+
+					수량에 왜 더 많은 단위가 입력이 안돼?
+				</a>
+			</h4>
+		</div>
+		<div id="collapse9" class="panel-collapse collapse">
+			<div class="panel-body">
+
+				인쇄 업체마다 제공하는 수량이 다 다르기 때문에, 우리는 효과적인 가격비교를 위해 가장 보편적인 수량을 제공하고 있어.
+
+			</div>
+		</div>
+	</div>
+	<div class="panel panel-default">
+		<div class="panel-heading">
+			<h4 class="panel-title">
+				<a data-toggle="collapse" data-parent="#accordion2" href="#collapse10">
+
+					제공되는 옵션 외의 옵션은 어떻게 해?
+				</a>
+			</h4>
+		</div>
+		<div id="collapse10" class="panel-collapse collapse">
+			<div class="panel-body">
+
+				특정 옵션들을 제공하는 업체가 충분치 않아서 가격비교가 어려웠기 때문에 기각한 옵션들이 있어. 이 옵션을 제공하는 업체는 직접 찾아보는 편이 빠를거야!
+
+			</div>
+		</div>
+	</div>
+	<div class="panel panel-default">
+		<div class="panel-heading">
+			<h4 class="panel-title">
+				<a data-toggle="collapse" data-parent="#accordion2" href="#collapse11">
+
+					어떻게 옵션 추천을 받을 수 있어?
+				</a>
+			</h4>
+		</div>
+		<div id="collapse11" class="panel-collapse collapse">
+			<div class="panel-body">
+
+				더 빠르고 편리한 이용을 위해 우리는 옵션 추천도 해줘! [인쇄 중수]나 [인쇄 하수]의 검색 페이지에서 용도를 입력하면, 그 용도에 적합한 옵션을 자동으로 추천해줄거야. [인쇄 하수]는 새로고침을 하며 여러 옵션 추천을 확인할 수 있어!
+
+			</div>
+		</div>
+	</div>
+	<div class="panel panel-default">
+		<div class="panel-heading">
+			<h4 class="panel-title">
+				<a data-toggle="collapse" data-parent="#accordion2" href="#collapse12">
+
+					옵션 추천의 기준은 뭐야?
+				</a>
+			</h4>
+		</div>
+		<div id="collapse12" class="panel-collapse collapse">
+			<div class="panel-body">
+
+				우리는 수십개의 인쇄 업체의 자료를 토대로 본 용도에 가장 보편적인 옵션을 추천하도록 했어.
+
+			</div>
+		</div>
+	</div>
+	<div class="panel panel-default">
+		<div class="panel-heading">
+			<h4 class="panel-title">
+				<a data-toggle="collapse" data-parent="#accordion2" href="#collapse13">
+
+					'요약글'은 뭐야?
+				</a>
+			</h4>
+		</div>
+		<div id="collapse13" class="panel-collapse collapse">
+			<div class="panel-body">
+
+				요약글은 옵션 선택 페이지의 하단에 나오는 글이자, 결과 페이지의 상단에 나오는 글이야. ‘요약글’은 선택된 옵션을 하나의 문장으로 요약해주지. 주문을 할때 그대로 읽거나 붙여넣으면 되니까 참 편리하지?
+
+			</div>
+		</div>
+	</div>
+	<div class="panel panel-default">
+		<div class="panel-heading">
+			<h4 class="panel-title">
+				<a data-toggle="collapse" data-parent="#accordion2" href="#collapse14">
+
+					[인쇄 하수] 페이지의 '다른 조합 보기' 버튼은 뭐야?
+				</a>
+			</h4>
+		</div>
+		<div id="collapse14" class="panel-collapse collapse">
+			<div class="panel-body">
+
+				우리가 추천하는 옵션들이 마음에 안 들 경우, ‘다른 조합 보기’를 클릭해서 다른 추천 옵션들을 볼 수 있어!
+
+			</div>
+		</div>
+	</div>
+</div>
+
+
+  </div>
+  <div id="menu2" class="tab-pane fade">
+
+		<div class="panel-group" id="accordion3">
+	<div class="panel panel-default">
+		<div class="panel-heading">
+			<h4 class="panel-title">
+				<a data-toggle="collapse" data-parent="#accordion3" href="#collapse15">
+
+					'더 저렴하게?'는 뭐야?
+				</a>
+			</h4>
+		</div>
+		<div id="collapse15" class="panel-collapse collapse in">
+			<div class="panel-body">
+
+				흔한 일은 아니지만, 매수를 늘렸을 때 총액이 같거나 심지어 더 저렴해질 때가 있어! 그런 꿀찬스를 놓치지 말라고 우리가 그때그때 찾아서 제안을 해줄거야.
+			</div>
+		</div>
+	</div>
+	<div class="panel panel-default">
+		<div class="panel-heading">
+			<h4 class="panel-title">
+				<a data-toggle="collapse" data-parent="#accordion3" href="#collapse16">
+
+					'이건 어때요?'는 뭐야?
+				</a>
+			</h4>
+		</div>
+		<div id="collapse16" class="panel-collapse collapse">
+			<div class="panel-body">
+
+				다른 옵션이 같을때 매수를 늘렸을 때 장당 가격이 저렴해질 때가 많아. 그런 기회를 놓치면 아쉬울 것 같아서 우리는 그때그때 찾아서 제안을 해줄거야.
+			</div>
+		</div>
+	</div>
+	<div class="panel panel-default">
+		<div class="panel-heading">
+			<h4 class="panel-title">
+				<a data-toggle="collapse" data-parent="#accordion3" href="#collapse17">
+
+					인쇄업체와 주문 연동이 돼?
+				</a>
+			</h4>
+		</div>
+		<div id="collapse17" class="panel-collapse collapse">
+			<div class="panel-body">
+
+				아쉽지만 아직까지 자동연동이 되고 있지는 않아. 하지만 업체 전화번호가 결과 페이지에 나오니 ‘요약글’ 그대로 주문하면 일사천리겠지? 홈페이지 링크도 제공하니까 클릭해서 원하는 페이지에 바로 들어갈 수 있어!
+
+			</div>
+		</div>
+	</div>
+
+	</div>
+
+
+
+  </div>
+	  <div id="menu3" class="tab-pane fade">
+			<div class="panel-group" id="accordion4">
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h4 class="panel-title">
+					<a data-toggle="collapse" data-parent="#accordion4" href="#collapse18">
+
+
+						등록된 업체인데, 정보가 잘 못 나와있어. 수정이 가능해?
+					</a>
+				</h4>
+			</div>
+			<div id="collapse18" class="panel-collapse collapse ">
+				<div class="panel-body">
+
+
+					안녕하세요, 김사원입니다. 귀하의 업체 정보에 대한 오류가 있는 점에 대해 사과드립니다. 정보를 제공해주신다면 빠른 시일내로 업데이트하도록 하겠습니다. 감사합니다!
+				</div>
+			</div>
+		</div>
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h4 class="panel-title">
+					<a data-toggle="collapse" data-parent="#accordion4" href="#collapse19">
+
+
+
+						미등록된 업체인데, 제휴를 맺고 싶어. 가능해?
+					</a>
+				</h4>
+			</div>
+			<div id="collapse19" class="panel-collapse collapse ">
+				<div class="panel-body">
+
+
+
+					안녕하세요, 김사원입니다. 카톡으로 연락주시면 개별연락 드리겠습니다! 감사합니다!
+				</div>
+			</div>
+		</div>
+
+
+	</div>
+
+
+		</div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
 	</div>
 
 
