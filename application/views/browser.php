aa66cbde35fa5f70fbe67890a881adc302c0a98d
Woohyun Kim <r45r54r45@naver.com>
2016-01-12 13:36:13 +0900

    fwef

--- application/views/browser.php
+++ application/views/browser.php
@@ -4,7 +4,7 @@
   </div>
   <div class="row">
     <div class="col-xs-12">
-      <h2 style="text-align:center;">모바일 페이지는 현재 개발 중입니다.<br> 데스크탑으로 접속하시면 모든 기능을 이용할 수 있습니다.</h2>
+      <h2 style="text-align:center;">현재 사용하고 계신 브라우저에서는 이용할 수 없습니다.<br> 크롬브라우저로 접속하시면 모든 기능을 이용할 수 있습니다.</h2>
     </div>
   </div>
 </div>
